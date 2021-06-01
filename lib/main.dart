613a2f998623d533d9506886af16676d63e0df5f
Trevor Settles <roboteng@gmail.com>
2021-06-01 14:52:15 -0600

    attempting fail

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
       body: Center(
-        child: Text("Changed!"),
+        child: FlatButton(
+          child: Text("Fail on purpose"),
+        ),
       ),
     ),
   ));
